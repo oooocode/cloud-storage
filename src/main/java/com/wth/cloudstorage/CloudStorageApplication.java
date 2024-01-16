a39ad6248258f8f0c0bd51bd56e44d8be89e4114
Wth <29977@qq.com>
2024-01-16 23:12:34 +0800

    项目初始化

---
+++ src/main/java/com/wth/cloudstorage/CloudStorageApplication.java
@@ -0,0 +1,16 @@
+package com.wth.cloudstorage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author: wth
+ * @Create: 2024/1/16 - 22:23
+ */
+@SpringBootApplication
+public class CloudStorageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudStorageApplication.class);
+    }
+}
